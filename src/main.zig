81fb018a9b7e8fef059db8ab002fc415d7e2b99d
boyair <yair.zuriel1@gmail.com>
2024-10-20 20:29:04 +0300

    updated tasks

--- src/main.zig
+++ src/main.zig
@@ -67,17 +67,13 @@ pub fn main() !void {
 //TODO:
 //fix framedrops when holding fastforward key by:
 //1 - set a time space minimum for fastforwarding and undoing.
-//2 - save the textures needed to be made in a data structure instead of having to wain for a frame each time.
+//2 - save the textures needed to be made in a data structure instead of having to wait for a frame each time.
 //3 - if possible make a seperate renderer for the making of texture instead of using the main one (possibly best solution if the renderer can be made and used on the logic thread).
 //4 - only add the texture to the list of things to be drawn after its been created and then there is no need to wait.
 //
 //make a pointer (can only be allocated on the heap) with an arrow that points to the address
 //make pointer(^) arrows toggleable with a checkbox
-//make deinit/close functions for app, heap, stack an UI.
-//make the init and deinit (^) functions request an allocator (better for testing memory leaks)
-//optional: make a wrapper for the init and deinit functions (^) that already has an allocator.
-//optional: make allocation, search and free a single action.
 //start working on sound
 //move logic to another thread to avoid framerate limitation (have a tickrate sepertae from framerate)
 //improve ui element for current action. add arrows for fastforward/ undo
-//improve api to make it look more like regular code and not like a new language
+//improve api to make it look more like zig code and not like a new language
