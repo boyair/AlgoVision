6a0cdc7d47382bdc373c4d0bd457dc5eec9b0248
boyair <yair.zuriel1@gmail.com>
2024-12-02 23:41:15 +0200

    added function to clear array without clearing memory

--- src/std/array.zig
+++ src/std/array.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 const heap = @import("../heap/interface.zig");
-
 pub const Array = struct {
     const Self = @This();
     allocator: std.mem.Allocator,
@@ -10,10 +9,16 @@ pub const Array = struct {
     pub fn init(allocator: std.mem.Allocator) Self {
         return Self{ .allocator = allocator, .mem = &[_]usize{}, .capacity = 0 };
     }
+    pub fn deinit(self: *Self) void {
+        heap.free(self.allocator, self.mem);
+    }
     pub fn initWithCapacity(allocator: std.mem.Allocator, capacity: usize) Self {
         var list = Self{ .allocator = allocator, .mem = &[_]usize{}, .capacity = 0 };
         list.ensureMinCapacity(capacity);
         return list;
+    }
+    pub fn clearRetainingCapacity(self: *Self) void {
+        self.items = 0;
     }
     pub fn insert(self: *Self, item: i64) void {
         if (self.capacity == self.items) {
