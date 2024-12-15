df48e31568f69d63fc7919dd2182d2c4c83f655a
boyair <yair.zuriel1@gmail.com>
2024-12-15 15:40:31 +0200

    recreated build file to make it easy to use on both linux and windows

--- build.zig
+++ build.zig
@@ -1,79 +1,90 @@
 const std = @import("std");
-const Sdk = @import("SDL");
-const builtin = @import("builtin");
-
-var asset_path: std.Build.LazyPath = undefined;
-var mixerdll: std.Build.LazyPath = undefined;
-var imagedll: std.Build.LazyPath = undefined;
-var sdk: *Sdk = undefined;
+pub const Sdk = @import("SDL");
+var asset_path: std.fs.Dir = undefined;
+var module: *std.Build.Module = undefined;
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    sdk = Sdk.init(b, null, null);
-    asset_path = b.path("assets/");
-    b.installDirectory(.{ .source_dir = asset_path, .install_dir = .{ .bin = {} }, .install_subdir = "./" });
-
-    const lib = b.addStaticLibrary(.{
-        .name = "AlgoVision",
+    module = b.addModule("AlgoVision", .{
         .root_source_file = b.path("src/app.zig"),
         .target = target,
         .optimize = optimize,
     });
-    asset_path = b.path("assets/");
-    lib.root_module.addImport("SDL", sdk.getWrapperModule());
-    b.installArtifact(lib);
-    mixerdll = b.path("windows/bin/SDL2_mixer.dll");
-    imagedll = b.path("windows/bin/SDL2_image.dll");
-    if (target.result.os.tag == .windows) {
-        b.installBinFile("windows/bin/SDL2_mixer.dll", "./SDL2_mixer.dll");
-        b.installBinFile("windows/bin/SDL2_image.dll", "./SDL2_image.dll");
-        linkWindows(lib);
+    asset_path = std.fs.openDirAbsolute(std.fs.path.join(b.allocator, &.{ b.build_root.path.?, "assets/" }) catch {
+        @panic("failed to allocate memory for asset path");
+    }, .{ .iterate = true }) catch {
+        @panic("unable to open asset directory");
+    };
+}
+pub fn fullInstall(b: *std.Build, exe: *std.Build.Step.Compile, module_name: []const u8) void {
+    const mod = getModule(b);
+    exe.root_module.addImport(module_name, mod);
+    linkSDL(b, exe);
+    installAssets(b) catch {
+        @panic("failed to install assets");
+    };
+}
+
+pub fn installAssets(b: *std.Build) !void {
+    const bin_directory: std.fs.Dir = try std.fs.openDirAbsolute(try std.fs.path.join(b.allocator, &.{ b.install_path, "bin" }), .{});
+    var walker = try asset_path.walk(b.allocator);
+    defer walker.deinit();
+    while (try walker.next()) |entry| {
+        switch (entry.kind) {
+            .directory => {
+                // Create subdirectory in destination
+                try bin_directory.makePath(entry.path);
+            },
+            .file => {
+                // Copy file from source to destination
+                try asset_path.copyFile(entry.path, bin_directory, entry.path, .{});
+            },
+            else => continue,
+        }
     }
 }
 
-fn getWinDLLs(b: *std.Build) void {
-    b.installBinFile("windows/bin/SDL2_mixer.dll", "./SDL2_mixer.dll");
-    b.installBinFile("windows/bin/SDL2_image.dll", "./SDL2_image.dll");
+pub fn getModule(b: *std.Build) *std.Build.Module {
+    const sdk = Sdk.init(b, null, null);
+    module.addImport("SDL", sdk.getWrapperModule());
+    module.addIncludePath(b.path("windows/include/"));
+    return module;
 }
 
-fn linkWindows(exe: *std.Build.Step.Compile) void {
-    exe.linkSystemLibrary("mingw32");
-    exe.linkSystemLibrary("SDL2main");
-    exe.linkSystemLibrary("SDL2");
-    exe.linkSystemLibrary("SDL2_ttf");
-    exe.linkSystemLibrary("SDL2_mixer");
-    exe.linkSystemLibrary("SDL2_image");
-    exe.linkSystemLibrary("user32");
-    exe.linkSystemLibrary("gdi32");
-    exe.linkSystemLibrary("winmm");
-    exe.linkSystemLibrary("imm32");
-    exe.linkSystemLibrary("ole32");
-    exe.linkSystemLibrary("oleaut32");
-    exe.linkSystemLibrary("version");
-    exe.linkSystemLibrary("uuid");
-    exe.linkSystemLibrary("shell32");
-    exe.linkSystemLibrary("setupapi");
-    exe.subsystem = .Windows;
-}
-pub fn getAssets(b: *std.Build) void {
-    b.installDirectory(.{
-        .source_dir = asset_path,
-        .install_dir = .{ .bin = {} },
-        .install_subdir = "./",
-    });
-}
+pub fn linkSDL(b: *std.Build, exe: *std.Build.Step.Compile) void {
+    const target_os = exe.rootModuleTarget().os.tag;
+    const sdk = Sdk.init(b, null, null);
+    exe.root_module.addImport("SDL", sdk.getWrapperModule());
+    if (target_os == .windows) {
+        b.installBinFile("windows/bin/SDL2_mixer.dll", "./SDL2_mixer.dll");
+        b.installBinFile("windows/bin/SDL2_image.dll", "./SDL2_image.dll");
+        exe.subsystem = .Windows;
 
-pub fn linkSDL(exe: *std.Build.Step.Compile) void {
-    sdk.link(exe, .dynamic, .SDL2); // link SDL2 as a static library
-    sdk.link(exe, .dynamic, .SDL2_ttf); // link SDL2_ttf as a static library
-    exe.linkSystemLibrary("SDL2_image");
-    exe.linkSystemLibrary("SDL2_mixer");
-    exe.linkLibC();
-    //  if (target.result.os.tag == .windows) {
-    //      b.installBinFile("windows/bin/SDL2_mixer.dll", "./SDL2_mixer.dll");
-    //      b.installBinFile("windows/bin/SDL2_image.dll", "./SDL2_image.dll");
-    //      exe.subsystem = .Windows;
-    //      linkWindows(exe);
-    //  }
+        //exe.root_module.addImport("SDL", )
+        sdk.link(exe, .dynamic, .SDL2); // link SDL2 as a static library
+        sdk.link(exe, .dynamic, .SDL2_ttf); // link SDL2_ttf as a static library
+        exe.linkSystemLibrary("mingw32");
+        exe.linkSystemLibrary("SDL2main");
+        exe.linkSystemLibrary("SDL2");
+        exe.linkSystemLibrary("SDL2_ttf");
+        exe.linkSystemLibrary("SDL2_mixer");
+        exe.linkSystemLibrary("SDL2_image");
+        exe.linkSystemLibrary("user32");
+        exe.linkSystemLibrary("gdi32");
+        exe.linkSystemLibrary("winmm");
+        exe.linkSystemLibrary("imm32");
+        exe.linkSystemLibrary("ole32");
+        exe.linkSystemLibrary("oleaut32");
+        exe.linkSystemLibrary("version");
+        exe.linkSystemLibrary("uuid");
+        exe.linkSystemLibrary("shell32");
+        exe.linkSystemLibrary("setupapi");
+        exe.linkLibC();
+    } else {
+        sdk.link(exe, .static, .SDL2); // link SDL2 as a static library
+        sdk.link(exe, .static, .SDL2_ttf); // link SDL2_ttf as a static library
+        exe.linkSystemLibrary("SDL2_image");
+        exe.linkSystemLibrary("SDL2_mixer");
+    }
 }
